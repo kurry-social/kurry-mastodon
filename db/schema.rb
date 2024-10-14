afb173fa3d927375212732b9eca9e2d7c9b6373c
perillamint <perillamint@silicon.moe>
2024-10-14 16:25:14 +0900

    policy: do not silence private mentions by default.

--- db/schema.rb
+++ db/schema.rb
@@ -731,7 +731,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_27_132728) do
     t.integer "for_not_following", default: 0, null: false
     t.integer "for_not_followers", default: 0, null: false
     t.integer "for_new_accounts", default: 0, null: false
-    t.integer "for_private_mentions", default: 1, null: false
+    t.integer "for_private_mentions", default: 0, null: false
     t.integer "for_limited_accounts", default: 1, null: false
     t.index ["account_id"], name: "index_notification_policies_on_account_id", unique: true
   end
